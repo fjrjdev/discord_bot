22904be5371371ca602788d5bc75d5baa3ab697e
fjrjdev <thedromemafia@gmail.com>
2022-11-15 21:57:53 -0300

    update ignore

--- main.py
+++ main.py
@@ -1,61 +1,161 @@
-import json
-import os
-import random
+import asyncio
 
 import discord
-import dotenv
-import requests
+import youtube_dl
+import os
 
+from discord.ext import commands
+import dotenv
 
 dotenv.load_dotenv()
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ""
+
+
+ytdl_format_options = {
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    "options": "-vn",
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=not stream)
+        )
+
+        if "entries" in data:
+            # take first item from a playlist
+            data = data["entries"][0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        """Joins a voice channel"""
+
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+
+    @commands.command()
+    async def play(self, ctx, *, query):
+        """Plays a file from the local filesystem"""
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(
+            source, after=lambda e: print(f"Player error: {e}") if e else None
+        )
+
+        await ctx.send(f"Now playing: {query}")
+
+    @commands.command()
+    async def yt(self, ctx, *, url):
+        """Plays from a url (almost anything youtube_dl supports)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(
+                player, after=lambda e: print(f"Player error: {e}") if e else None
+            )
+
+        await ctx.send(f"Now playing: {player.title}")
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """Streams from a url (same as yt, but doesn't predownload)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(
+                player, after=lambda e: print(f"Player error: {e}") if e else None
+            )
+
+        await ctx.send(f"Now playing: {player.title}")
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        """Changes the player's volume"""
+
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @commands.command()
+    async def stop(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.disconnect()
+
+    @play.before_invoke
+    @yt.before_invoke
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    description="Relatively simple music bot example",
+    intents=intents,
+)
 
 
-def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]["q"] + " -" + json_data[0]["a"]
-    return quote
-
-
-def get_find(param):
-    response = requests.get(f"https://simple-spotify-api.herokuapp.com/?search={param}")
-    json_data = json.loads(response.text)
-    print(json_data["data"][0]["music_url"])
-    return json_data["data"][0]["music_url"]
-
-
-meme_words = ["Drake", "Is this a...?", "This is fine"]
-res_words = ["Ta loco", "Hotlime bling"]
-
-
-@client.event
+@bot.event
 async def on_ready():
-    print("we have llogged in as{0.user}".format(client))
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    msg = message.content
-    if msg.startswith("$hello"):
-        await message.channel.send("Hello Word!")
-
-    if msg.startswith("$inspire"):
-        quote = get_quote()
-        await message.channel.send(quote)
-    if msg.startswith("$find"):
-        search = msg[6::]
-        response = get_find(search)
-        await message.channel.send(response)
-
-    if any(word in msg for word in meme_words):
-        await message.channel.send(random.choice(res_words))
+async def main():
+    async with bot:
+        await bot.add_cog(Music(bot))
+        await bot.start(os.getenv("TOKEN"))
 
 
-client.run(os.getenv("TOKEN"))
+asyncio.run(main())
